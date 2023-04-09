39baeddfbeaf9f71c97db014effeac7811e090bb
akromah12 <45060593+akromah12@users.noreply.github.com>
2023-04-09 20:16:05 +0000

    created a test file using pytest
    After running the test, pytest displays a summary of the test results, including which tests passed and which ones failed.

---
+++ test_main.py
@@ -0,0 +1,18 @@
+from main import addition
+from multiplication import multiply
+from subtraction import subtract
+
+def test_add_positive():
+    assert addition(5,2) == 7
+
+def test_add_negative():
+    assert addition(10,-40) == -30
+
+def test_add_zeros():
+    assert addition(2,0) == 2
+
+def test_multiplication():
+    assert multiply(2,2) == 2
+
+def test_subtraction():
+    assert subtract(300,20) == 290
