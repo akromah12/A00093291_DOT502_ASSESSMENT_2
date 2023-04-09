56347072e1588b2cb7d1b196421830d907a525c5
akromah12 <samuelerzoah@gmail.com>
2023-04-09 19:15:35 +0000

    Created a simple Maths Calculator

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a simple maths calculator 
+
+def addition(a,b):
+    return (a+b)
+
+# This code allows a user to input numbers
+if __name__ == "__main__":
+    firstNumber = float(input("Please Enter first number: "))
+    secondNumber = float(input("Please Enter second number: "))
+
+    # The out of the two numbers
+    print("The summation of the two numbers is:",addition(firstNumber,secondNumber))
+
+
+    
+
+
